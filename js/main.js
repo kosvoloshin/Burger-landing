f2e2b08eb2ee55ad9f22eadea89a12f1156aeb92
Voloshin Konstantin <23.03@i.ua>
2018-02-04 12:32:23 +0200

    add maps

--- js/main.js
+++ js/main.js
@@ -12,28 +12,28 @@ function initMap() {
         fullscreenControl: false,
         center: uluru
     });
-    var image = '../img/content/map-marker.png';
+    var image = './img/content/map-marker.png';
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(59.94554327989287, 30.38935262114668),
         map: map,
         icon: image
     });
 
-    var image = '../img/content/map-marker.png';
+    var image = './img/content/map-marker.png';
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(59.91142323563909, 30.50024587065841),
         map: map,
         icon: image
     });
 
-    var image = '../img/content/map-marker.png';
+    var image = './img/content/map-marker.png';
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(59.88693161784606, 30.319658102103713),
         map: map,
         icon: image
     });
 
-    var image = '../img/content/map-marker.png';
+    var image = './img/content/map-marker.png';
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(59.97033574821672, 30.315194906302924),
         map: map,
